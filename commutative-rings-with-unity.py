c7b9388aeeecf91757aa17cdff8873cc3102d789
Abhiram R <abhi.darkness@gmail.com>
2020-09-16 02:24:54 +0530

    Added Commutative Rings with Unity code

---
+++ commutative-rings-with-unity.py
@@ -0,0 +1,16 @@
+def multmod(x,y,z):
+    return (x*y)%z
+
+def mul_iden(set_ , modulo):
+    for i in range(0,len(set_)):
+        yes = True
+        for j in range(0,len(set_)):
+            if multmod(set_[j],set_[i],modulo)!=set_[j]:
+                yes = False
+                break
+        if yes:
+            iden = set_[i]
+            print("M.I = {}".format(iden))
+            return
+    print("No Inverse")
+

ec097b3b01eb302e7d8b178774505285e0c2add3
Abhiram R <abhi.darkness@gmail.com>
2020-09-16 03:00:13 +0530

    Added code to check for zero divisors

---
+++ zero_divisors.py
@@ -0,0 +1,14 @@
+# Zero Divisors
+
+def zero_divisors(set_,modulo):
+    for i in range(1,len(set_)):
+        yes = True
+        for j in range(1,len(set_)):
+            if multmod(set_[j],set_[i],modulo) == 0:
+                yes = False
+                break
+        if yes == False:
+            print("False for No Zero Divisors. Zero divisors exist so the given ring is not an integral domain")
+            return
+    if yes:
+        print("True for No Zero Divisors. No zero divisors exist so the given ring is an Integral domain")

77a6d37bf06b10dd8cdf5b82a778fb4d1deb18cf
Abhiram R <abhi.darkness@gmail.com>
2020-09-16 14:45:30 +0530

    Update multiplicatively_invertible.py

--- multiplicatively_invertible.py
+++ multiplicatively_invertible.py
@@ -1,6 +1,18 @@
 # MULTIPLICATIVE INVERSE
 
-from commutative_rings_with_unity import *
+def mul_iden(set_ , modulo):
+    for i in range(0,len(set_)):
+        yes = True
+        for j in range(0,len(set_)):
+            if multmod(set_[j],set_[i],modulo)!=set_[j]:
+                yes = False
+                break
+        if yes:
+            iden = set_[i]
+            print("Unity = {}".format(iden))
+            return iden
+    print("No Inverse")
+    return None
 
 def mult_invertible(set_, modulo):
     e = mul_iden(set_,modulo)
